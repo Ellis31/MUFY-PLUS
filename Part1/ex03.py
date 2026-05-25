2e76c08797a6cfb620aae43c77ecf43abc34c095
Ellis31 <ellisang33@gmail.com>
2026-05-25 05:24:23 +0000

    done

---
+++ Part1/ex03.py
@@ -0,0 +1,11 @@
+# Original text
+string1 = 'MaRviN THE bOt'
+string_lower = (string1.casefold())
+string_upper = (string1.upper())
+string_nospace = (string1.replace(" ",""))
+
+# Print results
+print(string1)
+print(string_lower)
+print(string_upper)
+print (string_nospace)
